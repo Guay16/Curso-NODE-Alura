dbf93147c00c4c166209c0bd27be4b7277aeae2b
Guay Pensak Miranda <guaypm36@gmail.com>
2023-11-15 11:34:33 -0300

    Aprendemos como passar props para um componente; Renderizamos listas com o .map; Trabalhamos com elementos aninhados usando a prop children; Ouvimos elementos disparados pelo DOM: o onSubmit do nosso form.

--- src/Componentes/CampoTexto/index.js
+++ src/Componentes/CampoTexto/index.js
@@ -2,17 +2,70 @@ import "./CampoTexto.css"
 
 const CampoTexto = (props) => {
 
-    /*const placeholderModificada = `${props.placeholder}...`*/
-
-  return (
+   return (
           <div className="campo-texto">
               <label>
                 {props.label}
                 </label>
-              <input placeholder= {props.placeholder}
+              <input required={props.obrigatorio}  placeholder= {props.placeholder}
               />
           </div>
              )
                         } 
    
 export default CampoTexto
+
+
+    /*
+    ************************** EXERCICIO *****************.
+
+Primeiro, ajuste o CampoTexto para receber as props relacionadas. Depois, crie o componente ListaSuspensa para permitir que o usuário selecione o time do colaborador.
+
+Em caso de dúvidas, não deixe de usar o fórum.import 
+
+
+******************'./CampoTexto.css'*****************************
+
+const CampoTexto = (props) => {
+
+    const placeholderModificada = `${props.placeholder}...` 
+
+    let valor = 'Guilherme Silveira'
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log(valor)
+    }
+
+    return (
+        <div className="campo-texto">
+            <label>
+                {props.label}
+            </label>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
+        </div>
+    )
+}
+
+export default CampoTexto
+
+**************************************ListaSuspensa/index.js***************************************
+
+import './ListaSuspensa.css'
+
+const ListaSuspensa = (props) => {
+    console.log(props.itens)
+
+    return (
+        <div className='lista-suspensa'>
+            <label>{props.label}</label>
+            <select required={props.required}>
+                {props.itens.map(item => {
+                    return <option key={item}>{item}</option>
+                })}
+            </select>
+        </div>
+    )
+}
+
+export default ListaSuspensa*/
--- src/Componentes/Formulario/index.js
+++ src/Componentes/Formulario/index.js
@@ -1,13 +1,35 @@
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../Botão'
 import './Formulario.css'
+
 const Formulario = ( )=> {
+    const times = [
+        'Programação',
+        'Front end',
+        'Data Science',
+        'DevOps',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão',
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido')
+    } 
+
             return (
                 <section className='formulario'>
-                    <form>
+                    <form onSubmit={aoSalvar} >
                         <h2> Preencha os dados abaixo para criar o card</h2>
-                          <CampoTexto label="Nome" placeholder ="Digite seu nome"/> 
-                          <CampoTexto label="Cargo" placeholder ="Digite seu cargo"/> 
-                          <CampoTexto label="Imagem" placeholder ="Digite o endereço da imagem"/> 
+                          <CampoTexto obrigatorio = {true} label="Nome" placeholder ="Digite seu nome"/> 
+                          <CampoTexto obrigatorio = {true} label="Cargo" placeholder ="Digite seu cargo"/> 
+                          <CampoTexto  label="Imagem" placeholder ="Digite o endereço da imagem"/> 
+                          <ListaSuspensa label= "Times" itens={times}/> 
+                          <Botao> CRIAR CARD </Botao> 
+                          
+                    
                     </form>
                 </section>
             )
